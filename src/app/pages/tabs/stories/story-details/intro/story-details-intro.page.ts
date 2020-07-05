f8682e9d8ac692d882c3ce2890ca96f3a007d1ba
Luiz Henrique Correia <luizhenriquecorreia1997@gmail.com>
2020-07-05 16:39:21 -0300

    feat(stories): add menu intro

--- src/app/pages/tabs/stories/story-details/intro/story-details-intro.page.ts
+++ src/app/pages/tabs/stories/story-details/intro/story-details-intro.page.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { Story } from './../../../../../models/story.interface';
 import { DbService } from './../../../../../services/db.service';
-import { map } from 'rxjs/operators';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-story-details-intro',
@@ -12,13 +12,46 @@ import { Observable } from 'rxjs';
   styleUrls: ['./story-details-intro.page.scss']
 })
 export class StoryDetailsIntroPage implements OnInit {
-  story$: Observable<Story>;
 
-  constructor(private router: Router, private dbService: DbService) {}
+  story$: Observable<Story>;
+  lists = [
+    {
+      'text': 'Escrever',
+      'label': 'Continue a história do seu jeito',
+      'image': 'assets/icon/writer.svg',
+      'url': '/writer'
+    },
+    {
+      'text': 'Fanfic',
+      'label': 'Crie sua história do zero',
+      'image': 'assets/icon/fanfic.svg',
+      'url': '/fanfic'
+    },
+    {
+      'text': 'Ilustrar',
+      'label': 'Ilustre a história com o seu desenho',
+      'image': 'assets/icon/ilustrater.svg',
+      'url': '/ilustrater'
+    },
+    {
+      'text': 'Gravar',
+      'label': 'A história com sua voz',
+      'image': 'assets/icon/recorder.svg',
+      'url': '/recorder'
+    },
+    {
+      'text': 'Colorir',
+      'label': 'Pinte a história com suas cores preferidas',
+      'image': 'assets/icon/painter.svg',
+      'url': '/painter'
+    }
+  ];
+
+  constructor(private router: Router, private dbService: DbService) { }
 
   ngOnInit() {
-    const id = this.router.url.split('/')[2];
-    this.story$ = this.dbService.doc$(`stories/${id}`).pipe(
+    const uid = this.router.url.split('/')[2];
+    this.story$ = this.dbService.doc$(`stories/${uid}`).pipe(
       map((data) => {
         data.content = data.content.split('\\n').join('\n');
         return data;
@@ -29,4 +62,5 @@ export class StoryDetailsIntroPage implements OnInit {
   navigateTo(uid: string) {
     this.router.navigate([`story-canvas/${uid}/drawing`]);
   }
+
 }
