aa9c446199cd5926c42335e47510ffeb24fbc6fd
patrickgdl <patrickgdlima@gmail.com>
2020-08-08 22:34:05 -0300

    styles(rating): fixing button view and dark mode

--- src/app/components/star-rating/star-rating.component.ts
+++ src/app/components/star-rating/star-rating.component.ts
@@ -1,10 +1,44 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
+/**inspired on https://github.com/melwinVincent/ionic4-star-rating */
+
 @Component({
   selector: 'star-rating',
-  templateUrl: './star-rating.component.html',
-  styleUrls: ['./star-rating.component.scss'],
+  template: `
+    <div class="rating">
+      <ion-button
+        fill="clear"
+        [id]="index"
+        [color]="index < this.Math.round(this.parseFloat(rating)) ? activeColor : defaultColor"
+        *ngFor="let index of iconsArray"
+        (click)="changeRating($event)"
+      >
+        <ion-icon
+          slot="icon-only"
+          [ngStyle]="{
+            'font-size': fontSize
+          }"
+          name="{{
+            halfStar && rating - index > 0 && rating - index <= 0.5
+              ? halfIcon
+              : index < this.Math.round(this.parseFloat(rating))
+              ? activeIcon
+              : defaultIcon
+          }}"
+        ></ion-icon>
+      </ion-button>
+    </div>
+  `,
+  styles: [
+    `
+      .rating ion-button {
+        --padding-end: 0;
+        --padding-start: 0;
+        --padding-top: 0;
+      }
+    `
+  ],
   providers: [
     {
       provide: NG_VALUE_ACCESSOR,
@@ -21,13 +55,13 @@ export class StarRatingComponent implements ControlValueAccessor, OnInit {
 
   @Output() ratingChanged: EventEmitter<number> = new EventEmitter<number>();
 
-  @Input() readonly = 'false';
-  @Input() activeColor = '#488aff';
-  @Input() defaultColor = '#aaaaaa';
-  @Input() activeIcon = 'ios-star';
-  @Input() defaultIcon = 'ios-star-outline';
-  @Input() halfIcon = 'ios-star-half';
-  @Input() halfStar = 'false';
+  @Input() readonly = false;
+  @Input() activeColor = 'success';
+  @Input() defaultColor = 'dark';
+  @Input() activeIcon = 'star';
+  @Input() defaultIcon = 'star-outline';
+  @Input() halfIcon = 'star-half';
+  @Input() halfStar = false;
   @Input() maxRating = 5;
   @Input() fontSize = '28px';
   @Input() public set rating(val: number) {
@@ -44,6 +78,7 @@ export class StarRatingComponent implements ControlValueAccessor, OnInit {
 
   ngOnInit() {
     this.rating = this.rating || 3; // default after input`s initialization
+
     for (let i = 0; i < this.maxRating; i++) {
       this.iconsArray.push(i);
     }
@@ -52,14 +87,17 @@ export class StarRatingComponent implements ControlValueAccessor, OnInit {
   writeValue(obj: number) {
     this.rating = obj;
   }
+
   registerOnChange(fn: any) {
     this.onChange = fn;
   }
+
   registerOnTouched(fn: any) {
     this.onTouched = fn;
   }
+
   setDisabledState?(isDisabled: boolean) {
-    this.readonly = isDisabled ? 'true' : 'false';
+    this.readonly = isDisabled ? true : false;
   }
 
   public get rating(): number {
@@ -72,20 +110,20 @@ export class StarRatingComponent implements ControlValueAccessor, OnInit {
   }
 
   changeRating(event: any) {
-    if (this.readonly && this.readonly === 'true') {
+    if (this.readonly) {
       return;
     }
     // event is different for firefox and chrome
     // tslint:disable-next-line: radix
     const id = event.target.id ? parseInt(event.target.id) : parseInt(event.target.parentElement.id);
 
-    if (this.halfStar && this.halfStar === 'true') {
+    if (this.halfStar) {
       this.rating = this.rating - id > 0 && this.rating - id <= 0.5 ? id + 1 : id + 0.5;
     } else {
       this.rating = id + 1;
     }
 
-    // unique event
+    // emitting event
     this.ratingChanged.emit(this.rating);
   }
 }
